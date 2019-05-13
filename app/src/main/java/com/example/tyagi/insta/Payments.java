72258ba4fe10436e79e90bf37e74506716382c49
Apoorv Tyagi <39600765+ApoorvTyagi@users.noreply.github.com>
2019-05-13 16:12:05 +0530

    Update Payments.java

--- app/src/main/java/com/example/tyagi/insta/Payments.java
+++ app/src/main/java/com/example/tyagi/insta/Payments.java
@@ -25,30 +25,23 @@ import org.json.JSONException;
 
 import java.math.BigDecimal;
 
-//Implementing click listener to our class
 public class Payments extends AppCompatActivity implements View.OnClickListener {
 
-    public static final String PAYPAL_CLIENT_ID = "AcHXiAM_Hu3vpdBD53yRrciWN5h01ZQpXiNzf8Pv9sQq4EZGg4UV8CM2eCp-gnAovIL45G0maz_1JQMZ";
+    public static final String PAYPAL_CLIENT_ID = "XXXXXXXXXX_XXXXXXXXXXXX_XXXXXXXXXXX";
 
     private FirebaseAuth firebaseAuth;
-    //The views
+
     private Button buttonPay;
     private EditText editTextAmount;
 
-    //Payment Amount
     private String paymentAmount;
 
 
-    //Paypal intent request code to track onActivityResult method
+    //Paypal intent request code 
     public static final int PAYPAL_REQUEST_CODE = 123;
 
 
-    //Paypal Configuration Object
-    private static PayPalConfiguration config = new PayPalConfiguration()
-            // Start with mock environment.  When ready, switch to sandbox (ENVIRONMENT_SANDBOX)
-            // or live (ENVIRONMENT_PRODUCTION)
-            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
-            .clientId(PAYPAL_CLIENT_ID);
+    private static PayPalConfiguration config = new PayPalConfiguration().environment(PayPalConfiguration.ENVIRONMENT_SANDBOX).clientId(PAYPAL_CLIENT_ID);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,41 +68,37 @@ public class Payments extends AppCompatActivity implements View.OnClickListener 
     }
 
     private void getPayment() {
-        //Getting the amount from editText
+
         paymentAmount = editTextAmount.getText().toString();
 
-        //Creating a paypalpayment
+
         PayPalPayment payment = new PayPalPayment(new BigDecimal(String.valueOf(paymentAmount)), "USD", "JoBot Payment",
                 PayPalPayment.PAYMENT_INTENT_SALE);
 
-        //Creating Paypal Payment activity intent
+
         Intent intent = new Intent(this, PaymentActivity.class);
 
-        //putting the paypal configuration to the intent
         intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
 
-        //Puting paypal payment to the intent
+
         intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
 
-        //Starting the intent activity for result
-        //the request code will be used on the method onActivityResult
         startActivityForResult(intent, PAYPAL_REQUEST_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //If the result is from paypal
+
         if (requestCode == PAYPAL_REQUEST_CODE) {
 
-            //If the result is OK i.e. user has not canceled the payment
             if (resultCode == Activity.RESULT_OK) {
-                //Getting the payment confirmation
+
                 PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
 
-                //if confirmation is not null
+
                 if (confirm != null) {
                     try {
-                        //Getting the payment details
+
                         String paymentDetails = confirm.toJSONObject().toString(4);
                         Log.i("paymentExample", paymentDetails);
 
@@ -119,13 +108,13 @@ public class Payments extends AppCompatActivity implements View.OnClickListener 
                                 .putExtra("PaymentAmount", paymentAmount));
 
                     } catch (JSONException e) {
-                        Log.e("paymentExample", "an extremely unlikely failure occurred: ", e);
+                        Log.e("paymentExample", "failure occurred: ", e);
                     }
                 }
             } else if (resultCode == Activity.RESULT_CANCELED) {
-                Log.i("paymentExample", "The user canceled.");
+                Log.i("paymentExample", "User canceled.");
             } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
-                Log.i("paymentExample", "An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+                Log.i("paymentExample", "An invalid Payment.");
             }
         }
     }
